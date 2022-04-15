651cb7a679c6802882b76cb3e5b9af75e7f7f836
gourav222 <67748004+gourav222@users.noreply.github.com>
2022-04-15 12:53:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,33 +3,41 @@ var app = express();
 var cors = require("cors");
 var bodyParser = require("body-parser");
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.use(cors());
 app.get("/",(req,res) => {
     res.send("Hello");
 })
-app.post("/",(req,res) => {
+app.post("/bfhl",(req,res) => {
     var a = {};
-    var body = req.body;
-    var name = "Gourav";
-    var prime = [];
-    var cnt = 0;
-    for(i = 0;i<body.numbers.length;i++){
-        var f = true;
-        for(j = 2;j<body.numbers[i];j++){
-            if(body.numbers[i] % j == 0)
-            {
-                f = false;
+    var status = true;
+    var User_Id = "gourav_parmar_28012001";
+    var colleage_Roll_Number = "0827CS191096"
+    var Email_Id = "gouravparmarcs19@acropolis.in"; 
+    var body = req.body.data;
+    var alpha = [];
+    var number = [];
+    var cnt1 = 0,cnt2 = 0;
+    str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    for(i = 0;i<body.length;i++){
+        var flag = false;
+        for(let j = 0; j < str.length; j++){
+            if(body[i] == str[j]){ 
+                flag = true;
                 break;
-            }
+            }    
         }
-        if(f === true){
-            prime[cnt++] = body.numbers[i];
+        if(flag){
+            alpha[cnt2++] = body[i];
+        }
+        if(!isNaN(body[i])){
+            number[cnt1++] = body[i];
         }
     }
-    a = {name:name,prime:prime};
+    var is_success = true;
+    a = {is_success:is_success,user_id:User_Id,email:Email_Id,roll_number:colleage_Roll_Number,numbers:number,alphabets:alpha};
     res.json(a);
 })
 app.listen(port);
